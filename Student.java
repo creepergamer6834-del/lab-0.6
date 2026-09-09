6b26fe097cd243f2c5f11278735d9340246342a6
Sebastian L <creepergamer6834@gmail.com>
2026-09-09 09:15:39 -0700

    a

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    private String name;
+    private String id;
+    private int grade;
+
+    public Student(String name) {
+
+    }
+
+    public Student(String name, int grade) {
+
+    }
+}
