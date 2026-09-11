2ef629a81bc519da33a8f79dd2b3ef480aaf25be
Sebastian L <creepergamer6834@gmail.com>
2026-09-11 13:40:32 -0700

    a

--- Student.java
+++ Student.java
@@ -4,10 +4,53 @@ public class Student {
     private int grade;
 
     public Student(String name) {
+        this.name = name;
+        this.grade = 10;
+        this.id = generateId();
+    }
 
+    public String generateId() {
+        int firstId = (int) (Math.random() * (899 - 100 + 1) + 100);
+        int secondId = (int) (Math.random() * (9999 - 1000 + 1) + 1000);
+        return firstId + "-" + secondId;
     }
 
     public Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+        this.id = generateId();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = this.name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String newId) {
+        id = this.id;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int newGrade) {
+        grade = this.grade;
+    }
+
+    public String toString() {
+        return name + "is in grade " + grade + " and has id " + id + ".";
+    }
+
+    public boolean equals(Student other) {
+        return name.equals(other.name) && id.equals(other.id) && grade == other.grade;
 
     }
 }
--- StudentTester.java
+++ StudentTester.java
@@ -1,3 +1,22 @@
 public class StudentTester {
-
+    public static void main(String[] args) {
+        Student a = new Student("John", 10);
+        System.out.println(a.toString());
+        System.out.println("Equal: " + a.equals(a));
+        Student b = new Student("Jane", 11);
+        System.out.println(b.toString());
+        System.out.println("Grade: " + b.getGrade());
+        System.out.println("Id: " + b.getId());
+        System.out.println("Equal: " + b.equals(a));
+        Student c = new Student("Evan", 10);
+        System.out.println("Name: " + c.getName());
+        System.out.println("Grade: " + c.getGrade());
+        System.out.println("Id: " + c.getId());
+        System.out.println("Equal: " + c.equals(b));
+        Student d = new Student("Peter", 12);
+        System.out.println("Name: " + d.getName());
+        System.out.println("Grade: " + d.getGrade());
+        System.out.println("Id: " + d.getId());
+        System.out.println("Equal: " + d.equals(a));
+    }
 }
