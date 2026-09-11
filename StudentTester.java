e20eebdce70cb2169d1a781ece12e298ed7673bc
Sebastian L <creepergamer6834@gmail.com>
2026-09-11 13:52:10 -0700

    a

--- StudentTester.java
+++ StudentTester.java
@@ -5,16 +5,16 @@ public class StudentTester {
         System.out.println("Equal: " + a.equals(a));
         Student b = new Student("Jane", 11);
         System.out.println(b.toString());
-        System.out.println("Equal: " + b.equals(b));
+        System.out.println("Equal: " + b.equals(a));
         Student c = new Student("Jack", 12);
         System.out.println("Name: " + c.getName());
         System.out.println("Grade: " + c.getGrade());
         System.out.println("Id: " + c.getId());
-        System.out.println("Equal: " + c.equals(c));
+        System.out.println("Equal: " + c.equals(b));
         Student d = new Student("Peter", 9);
         System.out.println("Name: " + d.getName());
         System.out.println("Grade: " + d.getGrade());
         System.out.println("Id: " + d.getId());
-        System.out.println("Equal: " + d.equals(d));
+        System.out.println("Equal: " + d.equals(c));
     }
 }
